44675bd3da7d6b22001d7b5a275767c7e815f294
aoirint <aoirint@gmail.com>
2021-07-18 14:58:23 +0900

    5 seconds cache

--- app/go.mod
+++ app/go.mod
@@ -4,4 +4,5 @@ go 1.16
 
 require (
   github.com/labstack/echo/v4 v4.4.0
+  github.com/go-redis/redis/v8 v8.11.0
 )
--- app/main.go
+++ app/main.go
@@ -2,10 +2,20 @@
 package main
 
 import (
+  "time"
   "net/http"
   "github.com/labstack/echo/v4"
   "github.com/labstack/echo/v4/middleware"
+  "context"
+  "github.com/go-redis/redis/v8"
 )
+
+var ctx = context.Background()
+var rdb = redis.NewClient(&redis.Options{
+  Addr: "redis:6379",
+  Password: "",
+  DB: 0,
+})
 
 func main() {
   e := echo.New()
@@ -13,11 +23,28 @@ func main() {
   e.Use(middleware.Logger())
   e.Use(middleware.Recover())
 
-  e.GET("/", hello)
+  e.GET("/", getval)
+  e.GET("/set", setval)
 
   e.Logger.Fatal(e.Start(":8000"))
 }
 
-func hello(c echo.Context) error {
-  return c.String(http.StatusOK, "Hello, World!")
+func getval(c echo.Context) error {
+  val, err := rdb.Get(ctx, "mykey").Result()
+  if err == redis.Nil {
+    return c.String(http.StatusOK, "no data (nil)")
+  } else if err != nil {
+      panic(err)
+  }
+
+  return c.String(http.StatusOK, val)
+}
+
+func setval(c echo.Context) error {
+  err := rdb.Set(ctx, "mykey", "hoge", 5 * time.Second).Err()
+  if err != nil {
+      panic(err)
+  }
+
+  return c.String(http.StatusOK, "OK")
 }
